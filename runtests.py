abc02461a7e6d16e71d9ae696c50d1f19222b415
Rohith Asrk <rohith.asrk@gmail.com>
2018-08-03 23:39:17 +0530

    Fix the incorrect app_label in runtests.py

--- runtests.py
+++ runtests.py
@@ -11,5 +11,5 @@ if __name__ == "__main__":
     from django.core.management import execute_from_command_line
     args = sys.argv
     args.insert(1, "test")
-    args.insert(2, "openwisp_network_topology")
+    args.insert(2, "openwisp_radius")
 execute_from_command_line(args)
