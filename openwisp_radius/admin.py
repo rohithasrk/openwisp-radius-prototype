cfd6595feb2c73429d6f10db7ca1809c6667db12
Rohith Asrk <rohith.asrk@gmail.com>
2018-08-09 01:01:58 +0530

    Add multitenancy filters to admin

--- openwisp_radius/admin.py
+++ openwisp_radius/admin.py
@@ -12,7 +12,9 @@ class RadiusCheckAdmin(MultitenantAdminMixin, AbstractRadiusCheckAdmin):
     pass
 
 
-RadiusCheckAdmin.fields.append('organization')
+RadiusCheckAdmin.list_display += ('organization',)
+RadiusCheckAdmin.list_filter += (('organization', MultitenantOrgFilter),)
+RadiusCheckAdmin.fields.insert(1, 'organization')
 
 
 @admin.register(RadiusReply)
@@ -20,13 +22,26 @@ class RadiusReplyAdmin(MultitenantAdminMixin, AbstractRadiusReplyAdmin):
     pass
 
 
+RadiusReplyAdmin.list_display += ('organization',)
+RadiusReplyAdmin.list_filter += (('organization', MultitenantOrgFilter),)
+
+
 @admin.register(RadiusAccounting)
 class RadiusAccountingAdmin(MultitenantAdminMixin, AbstractRadiusAccountingAdmin):
     pass
 
+
+RadiusAccountingAdmin.list_display += ('organization',)
+RadiusAccountingAdmin.list_filter += (('organization', MultitenantOrgFilter),)
+
+
 @admin.register(Nas)
 class NasAdmin(MultitenantAdminMixin, AbstractNasAdmin):
     pass
+
+
+NasAdmin.list_display += ('organization',)
+NasAdmin.list_filter += (('organization', MultitenantOrgFilter),)
 
 
 @admin.register(RadiusUserGroup)
@@ -34,9 +49,17 @@ class RadiusUserGroupAdmin(MultitenantAdminMixin, AbstractRadiusUserGroupAdmin):
     pass
 
 
+RadiusUserGroupAdmin.list_display += ('organization',)
+RadiusUserGroupAdmin.list_filter += (('organization', MultitenantOrgFilter),)
+
+
 @admin.register(RadiusGroupReply)
 class RadiusGroupReplyAdmin(MultitenantAdminMixin, AbstractRadiusGroupReplyAdmin):
     pass
+
+
+RadiusGroupReplyAdmin.list_display += ('organization',)
+RadiusGroupReplyAdmin.list_filter += (('organization', MultitenantOrgFilter),)
 
 
 @admin.register(RadiusGroupCheck)
@@ -44,9 +67,17 @@ class RadiusGroupCheckAdmin(MultitenantAdminMixin, AbstractRadiusGroupCheckAdmin
     pass
 
 
+RadiusGroupCheckAdmin.list_display += ('organization',)
+RadiusGroupCheckAdmin.list_filter += (('organization', MultitenantOrgFilter),)
+
+
 @admin.register(RadiusPostAuth)
 class RadiusPostAuthAdmin(MultitenantAdminMixin, AbstractRadiusPostAuthAdmin):
     pass
+
+
+RadiusPostAuthAdmin.list_display += ('organization',)
+RadiusPostAuthAdmin.list_filter += (('organization', MultitenantOrgFilter),)
 
 
 @admin.register(RadiusBatch)
@@ -54,9 +85,17 @@ class RadiusBatchAdmin(MultitenantAdminMixin, AbstractRadiusBatchAdmin):
     pass
 
 
+RadiusBatchAdmin.list_display += ('organization',)
+RadiusBatchAdmin.list_filter += (('organization', MultitenantOrgFilter),)
+
+
 @admin.register(RadiusProfile)
 class RadiusProfileAdmin(MultitenantAdminMixin, AbstractRadiusProfileAdmin):
     pass
+
+
+RadiusProfileAdmin.list_display += ('organization',)
+RadiusProfileAdmin.list_filter += (('organization', MultitenantOrgFilter),)
 
 
 class RadiusUserProfileInline(AbstractRadiusUserProfileInline):
